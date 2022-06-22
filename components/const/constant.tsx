32d02cff4b2f1d779a984baf1d937965199fcf92
jeongunlee <junlee712@hanyang.ac.kr>
2022-06-22 13:30:41 +0900

    add rate button

--- components/const/constant.tsx
+++ components/const/constant.tsx
@@ -17,6 +17,7 @@ export const INIT_FILST: FilterStateObj = {
         {'name':'at','value':'중'},
         {'name':'at','value':'오피스'},
         {'name':'at','value':'호텔'},
+        {'name':'rate','value':'고금리'}
     ],
     float :[
         {'name':'loanamt','value':[150000000000,427000000000]},
@@ -26,14 +27,15 @@ export const INIT_FILST: FilterStateObj = {
 export const INIT_LOANAMT:[number,number] = [15E8,427E8]
 
 // need to be fixed by real min max value
-export const MM_LOANAMT:[number,number] = [1E8,1E10]
+export const MM_LOANAMT:[number,number] = [1E8,1000E8]
 
 // labels for each filter (label : button1 button2 ...) | (label \n slider ---- )
 export const LABELS = {
-    'it' : '투자유형',
-    'seniorstr': '대출순위',
-    'at' : '자산유형',  // no avail yet
-    'loanamt' : '대출약정금' // no avail yet
+    'it' : '투자 유형',
+    'seniorstr': '대출 순위',
+    'at' : '자산 유형',
+    'loanamt' : '대출 약정금',
+    'rate': '금리 종류'
 }
 
 
