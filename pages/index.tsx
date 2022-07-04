f5c7d5741ea359a67dd692334a442e32bf0d522f
oncender <oncender@gmail.com>
2022-07-04 14:40:37 +0900

    style change, call model and show results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import MovingComponent from 'react-moving-text';
-import tableStyles from "../styles/Table.module.scss";
 import mainStyles from "../styles/main.module.scss";
 import GridLoader from 'react-spinners/GridLoader';
 import BigSelect from '../components/partials/p1CompBigSelectMain';
+import ResultContents from '../components/partials/p1CompResultContents';
+import HowItWorksContents from '../components/partials/p1CompHowItWorksContents';
+import axios from "axios";
 
 
 const Home: NextPage = () => {
-  const [loanPriority, setLoanPriority] = useState('선순위');
-  const [loanType, setLoanType] = useState('담보대출');
+  const [loanPriority, setLoanPriority] = useState('선');
+  const [loanType, setLoanType] = useState('담보');
   const [loading, setLoading] = useState(false);
+  const [modelResult, setModelResult] = useState()
+  const focusRef = useRef<HTMLDivElement>(null);
+  const goToDetailStr = "";
+  const API_URL="/api/v1/model/pred";
 
   const handleLoanPriority = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setLoanPriority(event.target.value);
@@ -20,11 +25,23 @@ const Home: NextPage = () => {
     setLoanType(event.target.value);
   }
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const min=1500, max=4500;
+  const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    await getModelData();
+    const min=1200, max=4000;
     const term = Math.floor(Math.random() * (max-min) + min) // 0.5 ~ 2.5 second
     setLoading(true)
     setTimeout(() => {setLoading(false)}, term)
+    focusRef.current!.scrollIntoView();
+  }
+
+  const getModelData = async () => {
+    let url = API_URL + `?seniorstr=${loanPriority}&loancls=${loanType}`;
+
+    await axios.get(url)
+    .then((res)=>{
+      setModelResult(res.data);
+    })
+    .catch(e => { console.log(e) });
   }
 
   const prioritySelectValue = [
@@ -35,6 +52,8 @@ const Home: NextPage = () => {
   const loanTypeSelectValue = [
     { key: '담보', value : '담보대출'},
     { key: 'PF', value : 'PF대출'},
+    { key: '부가세', value : '부가세대출'},
+    { key: '브릿지', value : '브릿지대출'},
     { key: '한도', value : '한도대출'}];
 
   return (
@@ -43,7 +62,7 @@ const Home: NextPage = () => {
         <div className="flex justify-center h-full bg-cover bg-center" 
           style={{backgroundImage: "url(background-01.png)"}}>
           <div className="lg:flex h-full w-full justify-around">
-            <div className="flex h-1/2 justify-center items-center lg:h-full">
+            <div className="flex h-1/2 justify-center items-center mt-20 md:mt-8 lg:mt-0 lg:h-full">
               <div className="flex-col">
                 <div className={mainStyles.mainTitle}>Next Loan</div>
                 <div className={mainStyles.mainDesc1}>Trained 937 loan case data-set in 160 funds</div>
@@ -58,13 +77,22 @@ const Home: NextPage = () => {
                   <BigSelect options={loanTypeSelectValue} handleChange={handleLoanType} />
                 </div>
                 <div className={mainStyles.mainDesc2}>적정이자가 궁금하신가요?</div>
-                <button className="bg-[#67FFBF] w-full h-12 text-2xl my-4 mb-6
-                sm:w-72 sm:h-16 sm:text-3xl sm:m-10 
-                md:w-96 md:h-20 md:text-4xl md:mb-20 
-                rounded-lg text-blue-900 transition hover:scale-110 hover:duration-150 hover:ease-in-out hover:bg-[#BAFBE0]"
-                  onClick={handleClick}>
-                  지금 확인해보기
-                </button>
+                <MovingComponent
+                  type="slideInFromRight"
+                  duration="2000ms"
+                  delay="0s"
+                  direction="normal"
+                  timing="ease"
+                  iteration="1"
+                  fillMode="none">
+                  <button className="bg-[#67FFBF] w-full h-12 text-2xl my-4 mb-10
+                  sm:w-72 sm:h-16 sm:text-3xl sm:m-10 
+                  md:w-96 md:h-20 md:text-4xl md:mb-20 
+                  rounded-lg text-blue-900 transition hover:scale-110 hover:duration-150 hover:ease-in-out hover:bg-[#BAFBE0]"
+                    onClick={handleClick}>
+                    지금 확인해보기
+                  </button>
+                </MovingComponent>
               </div>
             </div>
             
@@ -73,59 +101,21 @@ const Home: NextPage = () => {
       </div>
 
       <div className="w-full">
-        <div className="flex justify-center items-center">
-          {loading?
-          <div className="my-20 transition transform delay-150 duration-300">
-            <GridLoader loading={loading} color='#67FFBF' size={30} />
-          </div>
-          :
-          <div className={tableStyles.modelTable}>
-            <table>
-              <thead>
-              <tr>
-                <th>구분</th>
-                <th>고정</th>
-                <th>변동</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>은행</td>
-                <td>1.1 ~ 2.0</td>
-                <td>1.1 ~ 2.0</td>
-              </tr>
-              <tr>
-                <td>보험</td>
-                <td>1.1 ~ 2.0</td>
-                <td>1.1 ~ 2.0</td>
-              </tr>
-              <tr>
-                <td>기타</td>
-                <td>1.1 ~ 2.0</td>
-                <td>1.1 ~ 2.0</td>
-              </tr>
-              </tbody>
-            </table>
-            <p>⚠️ 경고문구 여기에 넣어서 잘 보이는지 확인해보기</p>
-            <div className="mt-8 text-right">
-              <MovingComponent
-                type="pulse"
-                duration="1500ms"
-                delay="1s"
-                direction="alternate"
-                timing="ease-in-out"
-                iteration="infinite"
-                fillMode="none">
-                <Link href={`/detail?it=${loanType}&seniorstr=${loanPriority}`}>
-                  <a className="text-[#45f0a8] text-3xl underline underline-offset-4 decoration-2">
-                    더 상세한 정보 확인하기 →
-                  </a>
-                </Link>
-              </MovingComponent>
+        <div className="flex flex-col">
+          <div className="flex justify-center items-center" ref={focusRef}>
+            {loading?
+            <div className="my-20 transition transform delay-150 duration-300">
+              <GridLoader loading={loading} color='#67FFBF' size={30} />
             </div>
+            :
+            <ResultContents 
+              loanType={loanType} 
+              loanPriority={loanPriority} 
+              modelPredict={modelResult!}/>
+            }
           </div>
-          }
         </div>
+        <HowItWorksContents />
       </div>
     </div>
   )
