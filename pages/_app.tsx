d6c7c2ccd0a7bcfc716fba11b5d78c63e022461e
oncender <oncender@gmail.com>
2022-07-18 14:50:35 +0900

    modify GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import "../styles/Slider.scss"
 import "../styles/Card.scss"
 import type {AppProps} from 'next/app'
 import Layout from '../components/Layout'
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 function MyApp({Component, pageProps}: AppProps) {
-            //     {/*<header>*/}
-            // {/*</header>*/}
-
+    usePageViews();
+    
     return (
             <Layout>
+                <GoogleAnalytics />
                 <Component {...pageProps} />
             </Layout>
     )
